c2350f9a1abf18a4934ce6e8a8470bebd3415b60
Arccoma <mass0@naver.com>
2022-11-14 14:38:30 +0900

    arduino sketch modify: relay shield test

--- examples/example_project/SerialCommunicationWithPC/SerialCommunicationWithPC.ino
+++ examples/example_project/SerialCommunicationWithPC/SerialCommunicationWithPC.ino
@@ -1,24 +1,27 @@
 /**********************************************************************
   케이프로 QGC베터리 경고 외부로 알람. (2022/10/12)
-  QGC(또는 mavlink_udp예제코드)로부터 시리얼 신호를 받으면 경고등(LED)를 켠다.
-  0x01:  QGC-> Arduino  :  Arduino recive message from QGC that Battery Warnig.Turn ON LED.
-  0x03:  Arduino -> QGC : (if nessesary)Arduino send message to QGC that LED is Turned Off.
-
+  QGC(또는 mavlink_udp+libserial예제코드)로부터 시리얼 신호를 받으면 경고등(LED)를 켠다.
+  '1':  QGC-> Arduino  :  Arduino recive message from QGC that Battery Warnig.Turn ON RED LED.
+  '2':  QGC-> Arduino  :  Arduino recive message from QGC that RTL event.Turn ON GREEN LED.
+  '3':  QGC-> Arduino  :  Arduino recive message from QGC that RC LOSS.Turn ON YELLOW LED.
+  4채널(4~7핀) 릴레이 쉴드 사용하여 LED제어. (4번 출력은 사용하지 않음) 
 *****************************************************************************************/
 
 //#define SERIAL_PRINT_DEBUG // 아두이노의 시리얼 모니터 디버깅시 주석 해제
 
-#define PLAY_TIME 1000 // LED On Time
+#define PLAY_TIME 3000 // LED On Time
 int incomingByte = 0;
-const int BATTERY_LOW_LED = 13; //RED
-const int RTL_ALARM_LED =   12; //GREEN
-const int RC_LOSS_LED =     11; //BLUE
+const int BATTERY_LOW_LED = 5; //RED
+const int RTL_ALARM_LED =   6; //GREEN
+const int RC_LOSS_LED =     7; //YELLOW
 
 int flgLowBattry = 0;
 int flgRTL        = 0;
+int flgRcLoss   =0;
 
 unsigned int cntLowBattery = 0;
 unsigned int cntRTL = 0;
+unsigned int cntRcLoss = 0;
 
 void alramLowBattery();
 void alramRTL();
@@ -42,17 +45,26 @@ void loop() {
     if ( /*0x02*/'2' == incomingByte )  { // RTL ALARM
       flgRTL = 1;
     }
+    if ( /*0x02*/'3' == incomingByte )  { // RC Loss ALARM
+      flgRcLoss = 1;
+    }
 #ifdef SERIAL_PRINT_DEBUG
     if ('1' == incomingByte ) {
       Serial.print("Recive data: ");
       Serial.println( incomingByte );
-      flgPlay = 1;
+      flgLowBattry = 1;
     }
     if ('2' == incomingByte ) {
       Serial.print("Recive data: ");
       Serial.println( incomingByte );
-      flgPlay = 1;
+      flgRTL = 1;
     }
+    if ('3' == incomingByte ) {
+      Serial.print("Recive data: ");
+      Serial.println( incomingByte );
+      flgRcLoss = 1;
+    }
+    
 #endif
 
   } // if (Serial.available() > 0)
@@ -67,11 +79,15 @@ void loop() {
     alramRTL();
   }
 
+  if (flgRcLoss)
+  {
+    alramRcLoss();
+  }
+
 }// void loop()
 
 
-void alramLowBattery()
-{
+void alramLowBattery(){
   if (0 == cntLowBattery)// 처음 한번만 실행
   {
     digitalWrite(BATTERY_LOW_LED, HIGH);
@@ -97,18 +113,32 @@ void alramLowBattery()
   }
 }
 
-void alramRTL()
-{
+void alramRTL(){
   if (0 == cntRTL) // 처음 한번만 실행
   {
     digitalWrite(RTL_ALARM_LED, HIGH);
   }
   cntRTL++;
 
-  if (/*PLAY_TIME*/10000 < cntRTL)// 일정 시간 동작 후 정지.
+  if (PLAY_TIME < cntRTL)// 일정 시간 동작 후 정지.
   {
     digitalWrite(RTL_ALARM_LED, LOW);
     cntRTL = 0;
     flgRTL = 0;
+  }
+}
+
+void alramRcLoss(){
+  if (0 == cntRcLoss) // 처음 한번만 실행
+  {
+    digitalWrite(RC_LOSS_LED, HIGH);
+  }
+  cntRcLoss++;
+
+  if (PLAY_TIME < cntRcLoss)// 일정 시간 동작 후 정지.
+  {
+    digitalWrite(RC_LOSS_LED, LOW);
+    cntRcLoss = 0;
+    flgRcLoss = 0;
   }
 }
